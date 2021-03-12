b8ee76d18f1829305caeb9ce7e5013186435cdc8
mayte <mayte@192.168.1.137>
2021-03-12 16:53:11 +0100

    Clase viernes, cambios en Inversiones por D&V

--- src/main/java/algestudiante/p32/Inversiones.java
+++ src/main/java/algestudiante/p32/Inversiones.java
@@ -4,32 +4,32 @@ import java.util.List;
 
 /**
  * Algoritmo Divide y Venceras
- * Complejidad O(nlogn), {a=, b=, k=1} <- Por division.		(a=b)
+ * Complejidad O(nlogn), {a=2, b=2, k=1} <- Por division con a=b^k
  */
 public class Inversiones {
 
 	private List<Integer> ranking;
-	private long inversiones;
 	
 	public Inversiones(List<Integer> ranking) {
 		this.ranking = ranking;
 	}
 
 	public long start() {
-		inversiones = 0;
-//		recursivo(0);
+		return invRecursivo(0, ranking.size()-1);
+	}
+	
+	private long invRecursivo(int i, int j) {
+		long inversiones = 0;
+		if (j-i <= 1) {
+			if (j-i == 1 && i < j && ranking.get(i).compareTo(ranking.get(j)) > 0)
+				inversiones++;
+			return inversiones;
+		}
+		int m = (i+j)/2;
+		inversiones += invRecursivo(i, m);
+		inversiones += invRecursivo(m+1, j);
+//		combina(i, m, m+1, j);	// Mergesort
 		return inversiones;
 	}
 	
-	private void recursivo(int j) {
-//		if (j < ranking.size()) {
-//			for (int i = 0; i < ranking.size(); i++) {
-//				if (j < i && ranking.get(j).compareTo(ranking.get(i)) > 0)
-//					inversiones++;
-//			}
-//			j++;
-//			recursivo(j);
-//		}
-	}
-
 }
